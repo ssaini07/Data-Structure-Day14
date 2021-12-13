06a1b398217accfb2fa6171b19de5c174af198b4
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-13 14:49:41 +0530

    second commit

---
+++ src/Data_Structure/LinkedList/LinkedList.java
@@ -0,0 +1,10 @@
+package Data_Structure.LinkedList;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Data_Structure/LinkedList/Node.java
@@ -0,0 +1,5 @@
+package Data_Structure.LinkedList;
+
+public class Node {
+
+}
